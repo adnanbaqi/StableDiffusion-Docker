5d12fb08d574273604cda79ce0604c4afacad982
Md Adnan Baqi <adnan.baki786@gmail.com>
2024-11-02 16:29:27 +0530

    Lora Added

--- services/download/download.sh
+++ services/download/download.sh
@@ -20,6 +20,7 @@ download() {
 }
 
 mkdir -vp /data/.cache \
+  /data/models/Lora\
   /data/embeddings \
   /data/config/ \
   /data/models/ \
@@ -29,7 +30,8 @@ mkdir -vp /data/.cache \
   /data/models/LDSR \
   /data/models/VAE  \
   /data/models/ControlNet \
-  /data/models/ControlNet/models
+  /data/models/ControlNet/models\
+  
 
 #####################################################################################################################################
 # Downloading and extracting sd-webui-controlnet
@@ -41,25 +43,12 @@ unzip /tmp/sd-webui-controlnet.zip -d /tmp
 
 echo "Copying files to /data/config/auto/extensions/sd-webui-controlnet..."
 mkdir -p /data/config/auto/extensions/sd-webui-controlnet
-# Assuming the folder structure inside the zip is 'sd-webui-controlnet-master' based on GitHub's convention
 cp -r /tmp/sd-webui-controlnet-master/* /data/config/auto/extensions/sd-webui-controlnet
 
 echo "Cleaning up..."
 rm -rf /tmp/sd-webui-controlnet.zip /tmp/sd-webui-controlnet-master
 ######################################################################################################################################
 ######################################################################################################################################
-echo "Downloading Mikubill/sd-webui-controlnet repository archive..."
-download "https://github.com/glucauze/sd-webui-faceswaplab/archive/refs/heads/main.zip" "/tmp/sd-webui-faceswaplab.zip"
 
-echo "Extracting files..."
-unzip /tmp/sd-webui-faceswaplab.zip -d /tmp
-
-echo "Copying files to /data/config/auto/extensions/sd-webui-faceswaplab..."
-mkdir -p /data/config/auto/extensions/sd-webui-faceswaplab
-cp -r /tmp/sd-webui-faceswaplab-main/* /data/config/auto/extensions/sd-webui-faceswaplab
-
-echo "Cleaning up..."
-rm -rf /tmp/sd-webui-faceswaplab.zip /tmp/sd-webui-faceswaplab-main
-######################################################################################################################################
 echo "Downloading, this might take a while..."
 aria2c -x 4 --disable-ipv6 --input-file /docker/links.txt --dir /data --continue
